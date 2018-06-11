0eb889610a1c9e9f5b69f81a844e0bb2f013d2b4
cassandradesro <cdesro10@gmail.com>
2018-06-10 22:27:30 -0400

    start
    start of proj

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,8 @@
 "use strict";
 
 console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+
+document.querySelector(".hamburger").addEventListener("click", function () {
+	document.querySelector("nav").classList.toggle("active");
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 console.log(`Hello World from app.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+
+document.querySelector(".hamburger").addEventListener("click", function(){
+	document.querySelector("nav").classList.toggle("active");
+})
